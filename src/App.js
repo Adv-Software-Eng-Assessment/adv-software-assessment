7d0d11ccfbb10e572c640b5addd23d78fba68203
aw3some-dev <olumidesofayo@yahoo.co.uk>
2023-03-14 15:33:41 +0000

    revert to d99dd787cdfdbe44074a3c82febe193eeba4761e

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
 
-import Login from './components/views/Login';
-import Register from './components/views/Register';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/views/Login';
+import Register from './components/views/Register'
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route path="/" render={routeProps => <Redirect to="/login" />} exact />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-      </Router>
+          <Router>
+      <Switch>
+        <Route path="/" component={<Redirect to="/login" />} exact />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+
+      </Switch>
+    </Router>
+     
     </div>
   );
 }
